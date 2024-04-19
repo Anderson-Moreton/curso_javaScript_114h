39190a6d738a3454c1e25c14d0b236d252143125
Anderson Moreton <anderson.moreton@gmail.com>
2024-04-19 22:52:58 +0100

    Introdução e revisão de lógica de programação.

--- 1_introducao/primeiro-exemplo.js
+++ 1_introducao/primeiro-exemplo.js
@@ -1,1 +1,20 @@
-console.log("Hello World!!");
+console.log("Javascript funcionando 2222");
+
+
+/*
+let nome = "Daniel"
+console.log(10 + 20)
+*/
+
+console.log("qualquer coisas")
+
+// uma unica linha
+
+let nome = "Daniel"; alert(nome)
+
+const hasAttrubite = ""
+
+
+
+
+
