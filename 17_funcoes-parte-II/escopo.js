cf1f22b4fcecad681e5cd84342ac373e5b2b2941
Anderson Moreton <anderson.moreton@gmail.com>
2024-05-13 23:54:13 +0100

    Funções e métodos.

--- 17_funcoes-parte-II/escopo.js
+++ 17_funcoes-parte-II/escopo.js
@@ -28,3 +28,21 @@ const obj = {
 }
 obj.teste()
 // obj.teste2()
+
+const btn = document.querySelector("button")
+    const _this = this
+
+    btn.addEventListener("click", function () {
+        console.log("declaraton")
+        console.log(this)
+        console.log(_this)
+        console.log(this === _this)
+     })
+
+
+    btn.addEventListener("click", () => {
+        console.log("arrow")
+        console.log(this)
+        console.log(_this)
+        console.log(this === _this)
+})
