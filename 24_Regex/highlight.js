1b078055c1f9205c9820db824b150a7ba9e72246
Anderson Moreton <anderson.moreton@gmail.com>
2024-06-25 22:14:17 +0100

    Regex.

---
+++ 24_Regex/highlight.js
@@ -0,0 +1,33 @@
+// document.querySelector("input").addEventListener("input", e => {
+//     const valor = e.target.value
+//     Array.from(document.querySelectorAll("p")).map(paragraph => {
+//         const paragraphText = paragraph.innerText
+//         const regex = new RegExp(valor, "gi")
+//         const htmlReplaced = paragraphText.replace(regex, result => {
+//             return `<span class="hgt">${result}</span>`
+//         })
+//         paragraph.innerHTML = htmlReplaced
+//     })
+
+// })
+
+const paragraphsOrigins = Array.from(document.querySelectorAll("p")).map(p => p.innerHTML)
+
+ document.querySelector("input").addEventListener("input", e => {
+    const valor = e.target.value
+    Array.from(document.querySelectorAll("p")).map((paragraph, i) => {
+        const paragraphHTML = paragraphsOrigins[i]
+        if (valor) {
+            const regex = new RegExp(valor, "gi")
+            const htmlReplaced = paragraphHTML.replace(regex, result => {
+                    return `<span class="hgt">${result}</span>`
+            })
+            console.log(htmlReplaced)
+            paragraph.innerHTML = htmlReplaced
+        } else {
+            paragraph.innerHTML = paragraphHTML
+        }
+
+    })
+
+})
