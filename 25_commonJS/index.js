f7696872b01ca30a7f1764c6e12047fe968e9300
Anderson Moreton <anderson.moreton@gmail.com>
2024-06-29 22:50:07 +0100

    Common JS.

--- 25_commonJS/index.js
+++ 25_commonJS/index.js
@@ -2,12 +2,21 @@ const fs = require("fs");
 const emoji = require("node-emoji");
 
 const mod1 = require("./modules/mod1")
-console.log("mod1")
-console.log(mod1.fn)
-console.log(mod1.fn())
+// console.log("mod1")
+// console.log(mod1.fn)
+// console.log(mod1.fn())
 
-fs.writeFile("text-txt", "Olá Mundo", err => {
-    if(err) throw err
-    console.log("Saved!", emoji.get("coffee"))
-})
+console.log(__dirname)
+console.log(__filename)
+
+const mod2 = require("./modules/mod2")
+const mod3 = require("./modules/mod3")
+
+console.log(mod2)
+console.log(mod3)
+
+// fs.writeFile("text-txt", "Olá Mundo", err => {
+//     if(err) throw err
+//     console.log("Saved!", emoji.get("coffee"))
+// })
 
