4d4c47836018c69334d881c60eb4ac53599ca235
Anderson Moreton <anderson.moreton@gmail.com>
2024-07-03 00:48:46 +0100

    typescript.

---
+++ 27_typeScript/types/src/object.ts
@@ -0,0 +1,7 @@
+let aluno: { nome: string,  aprovado?: boolean, idade: number, notas: number[] }
+
+aluno = {
+    nome: "Anderson Moreton",
+    idade: 36,
+    notas: [7, 8, 9, 8]
+}
---
+++ 27_typeScript/types/src/tuple.ts
@@ -0,0 +1,7 @@
+let aluno2: { nome: string,  aprovado?: boolean, idade: number, notas:[number, number, number, number] }
+
+aluno2 = {
+    nome: "Anderson Moreton",
+    idade: 36,
+    notas: [7, 8, 9, 8]
+}
