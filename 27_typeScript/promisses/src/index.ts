df7c2b940d7938d202fd0a92b22a7f0e58abbe63
Anderson Moreton <anderson.moreton@gmail.com>
2024-07-18 20:52:39 +0100

    typeScript.

--- 27_typeScript/promisses/src/index.ts
+++ 27_typeScript/promisses/src/index.ts
@@ -10,3 +10,9 @@ axios.get<Array<ResponseUser>>("https://localhost:3001/users").then((response) =
     const resposta = response.data[0]
     console.log(resposta)
 })
+
+const maria: ResponseUser = { name: "maria", id: 2, email: "maria.gostosa@gmail.com" }
+maria.id++
+axios.post<ResponseUser>("https://localhost:3001/users", maria).then( (response) => {
+    console.log(response.data.id)
+})
