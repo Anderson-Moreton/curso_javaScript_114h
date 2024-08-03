c86616d7f52af51180c25af18d528cd8748f940b
Anderson Moreton <anderson.moreton@gmail.com>
2024-08-03 11:03:46 +0100

    Node.js

--- 28_nodejs/introducao/global.js
+++ 28_nodejs/introducao/global.js
@@ -1,3 +1,17 @@
 globalThis.console.log("teste")
 console.log(__filename)
 console.log(__dirname)
+console.log(process.argv)
+
+// const [,, firstName, lastName] = process.argv
+// console.log(`O nome é: ${firstName} ${lastName}`)
+
+const getByFlag = flag => {
+    const indexOfFlag = process.argv.indexOf(flag)
+    return process.argv[indexOfFlag + 1]
+}
+
+const fistName = getByFlag("--firstname")
+const lastName = getByFlag("--lastname")
+
+console.log(`O nome é: ${fistName} ${lastName}`)
