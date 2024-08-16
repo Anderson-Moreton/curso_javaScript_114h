e3fd71aa23ebffe259a1febaf07a8f7765f2e114
Anderson Moreton <anderson.moreton@gmail.com>
2024-08-16 22:42:42 +0100

    node.js

--- 28_nodejs/routingExpress/server.js
+++ 28_nodejs/routingExpress/server.js
@@ -1,5 +1,31 @@
 const express = require("express")
+const tasks = require("./data/tasks.json")
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/tasks", (req, res) => {
+    res.send(tasks)
+})
+
+app.post("/tasks", (req, res) => {
+    console.log(req.body)
+    const {title, userId} = req.body
+
+    const newTask = {
+        title,
+        completed: false,
+        creatdAt: Date.now(),
+        updatedAt: null,
+        userId,
+        id: tasks[tasks.length - 1].id +1
+    }
+
+    tasks.push(newTask)
+    res.send(newTask)
+
+})
 
 app.listen(3001)
