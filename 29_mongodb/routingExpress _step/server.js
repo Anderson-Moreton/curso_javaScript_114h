78360a756707911367c7d72128f7d30d94aba92c
Anderson Moreton <anderson.moreton@gmail.com>
2024-08-25 01:19:03 +0100

    mongodb

--- 29_mongodb/routingExpress _step/server.js
+++ 29_mongodb/routingExpress _step/server.js
@@ -1,9 +1,41 @@
 require("dotenv").config()
 
-// const app = require('./src/app')
+const app = require('./src/app')
 
-// const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
-// app.listen(PORT)
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = process.env.MONGO_URL;
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'myFirstProject';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+
+  // the following code examples can be pasted here...
+  const insertResult = await collection.insertMany([{ task: "task1", completed: false }, { task: "task2", completed: true }]);
+    console.log('Inserted documents =>', insertResult);
+
+  return 'done.';
+}
+
+main()
+  .then(()=> {
+    app.listen(PORT)
+  })
+  .catch(console.error)
+  .finally(() => client.close());
+
+app.listen(PORT)
 
 console.log(process.env.BELUGA)
